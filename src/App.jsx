c54c1907bcd0e73603fbe6a341bcd7f2493a31ff
czurban <alexe213131@gmail.com>
2026-05-02 17:47:51 +0200

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
 import "./App.css";
+import { card } from "./card.js";
 
-function App() {}
+const Cards = ({ name, discription }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{discription}</p>
+    </div>
+  );
+};
+const Skills = ({ skill }) => {
+  return <li>{skill}</li>;
+};
+
+function App() {
+  return (
+    <div className="mainDiv">
+      <div>
+        <img src="" alt="avatar"></img>
+        <h2></h2>
+        <p></p>
+      </div>
+      <div>
+        <input type="text" placeholder="show ur skills"></input>
+        <div></div>
+      </div>
+      <div>
+        {card.map((parametr) => {
+          return (
+            <Cards
+              key={parametr.id}
+              name={parametr.name}
+              discription={parametr.discription}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ src/card.js
@@ -0,0 +1,5 @@
+export const card = [
+  { id: 0, name: "1", discription: "4" },
+  { id: 1, name: "2", discription: "5" },
+  { id: 2, name: "3", discription: "6" },
+];
