90b082b68e112a9e5dfe8f193f2ede955a56355e
czurban <alexe213131@gmail.com>
2026-05-03 12:28:33 +0200

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,80 @@
+import { useState } from "react";
 import "./App.css";
+import avatarImg from "./assets/ava.png";
 import { card } from "./card.js";
 
 const Cards = ({ name, discription }) => {
   return (
-    <div>
+    <div className="card-item">
       <h1>{name}</h1>
       <p>{discription}</p>
     </div>
   );
 };
-const Skills = ({ skill }) => {
-  return <li>{skill}</li>;
-};
 
 function App() {
+  const [skill, setSkill] = useState("");
+  const [allSkills, setAllSkills] = useState([]);
+
+  function handleInput(e) {
+    setSkill(e.target.value);
+  }
+
+  function addSkill() {
+    if (skill.trim() !== "") {
+      setAllSkills([...allSkills, skill]);
+      setSkill("");
+    }
+  }
+
   return (
     <div className="mainDiv">
-      <div>
-        <img src="" alt="avatar"></img>
-        <h2></h2>
-        <p></p>
+      <div className="top">
+        <img
+          className="img"
+          src={avatarImg}
+          alt="avatar"
+          width={350}
+          height={450}
+        />
+        <h2>Oleksii Khudolei</h2>
+        <p>Jestem eblanem</p>
       </div>
+
       <div>
-        <input type="text" placeholder="show ur skills"></input>
-        <div></div>
+        <div>
+          <input
+            value={skill}
+            type="text"
+            placeholder="show ur skills"
+            onChange={handleInput}
+          />
+          <button onClick={addSkill}>add</button>
+        </div>
+        <ul className="list">
+          {allSkills.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
-      <div>
-        {card.map((parametr) => {
-          return (
-            <Cards
-              key={parametr.id}
-              name={parametr.name}
-              discription={parametr.discription}
-            />
-          );
-        })}
+
+      <div className="list">
+        {card.map((parametr) => (
+          <Cards
+            key={parametr.id}
+            name={parametr.name}
+            discription={parametr.discription}
+          />
+        ))}
       </div>
-      <div>
-        <p></p>
-        <p></p>
-        <p></p>
+
+      <div className="list">
+        <p>git</p>
+        <p>linkedin</p>
+        <p>mess</p>
       </div>
     </div>
   );
 }
+
 export default App;
